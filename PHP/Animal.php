8978ca804b1eee6cb21b1759e34879423cbf1934
Mawyn NHEK <79303836+Nezertiam@users.noreply.github.com>
2021-02-27 19:54:17 +0100

    Add files via upload

---
+++ PHP/Animal.php
@@ -0,0 +1,65 @@
+<!-- 
+    Toute classe PHP possède une majuscule et s'appellera comme le fichier
+    De plus, un fichier ne possède qu'une classe !! Très important !
+-->
+
+<?php
+
+class Animal {
+
+    // Attributs ou variables d'instance ou propriétés
+    // portée de variables : public, protected et private
+    private $_nomAnimal;
+    private $_race;
+    private $_nbPattes;
+    private $_dateNaissance;
+    private $_aliments;
+
+    // Constructeur
+    public function __construct($nomAnimal, $race, $nbPattes, $dateNaissance) {
+        $this->_nomAnimal = $nomAnimal;
+        $this->_race = $race;
+        $this->_nbPattes = $nbPattes;
+        $this->_dateNaissance = $dateNaissance;
+        $this->_aliments = [];
+    }
+
+    // Getters (accesseurs) et setters (mutateurs)
+    public function getNomAnimal() {
+        return $this->_nomAnimal;
+    }
+
+    public function getRace() {
+        return $this->_race;
+    }
+
+    public function getNbPattes() {
+        return $this->_nbPattes;
+    }
+
+    public function getDateNaissance() {
+        return $this->_dateNaissance;
+    }
+
+    
+    public function setNomAnimal($nomAnimal) {
+        $this->_nomAnimal = $nomAnimal;
+    }
+
+    public function setrace($race) {
+        $this->_race = $race;
+    }
+
+    public function setnbPattes($nbPattes) {
+        $this->_nbPattes = $nbPattes;
+    }
+
+    public function setdateNaissance($dateNaissance) {
+        $this->_dateNaissance = $dateNaissance;
+    }
+
+
+
+
+
+}
